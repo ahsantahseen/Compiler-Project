eeb142d6de8c2075ab09486155e452d39e5c182c
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2021-12-18 01:00:17 +0500

    Getting Some Tokens

--- CompilerClass.java
+++ CompilerClass.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CompilerClass {
@@ -10,13 +11,95 @@ public class CompilerClass {
             Scanner fileReader = new Scanner(fileObject);
             while (fileReader.hasNextLine()) {
                 String data = fileReader.nextLine();
-                String tokens[] = data.split(" ");
-                for (int i = 0; i < tokens.length; i++) {
-                    System.out.println(tokens[i]);
+                String lines[] = data.split("\n");
+                for (int i = 0; i < lines.length; i++) {
+                    String lexeme[] = lines[i].split(" ");
+                    System.out.println(Arrays.toString(lexeme));
+                    for (int j = 0; j < lexeme.length; j++) {
+                        if (lexeme[j].equals("int")) {
+                            System.out.println("<" + lexeme[j].toString() + ">,<0>");
+                        } else if (lexeme[j].equals("char")) {
+                            System.out.println("<" + lexeme[j].toString() + ">,<1>");
+                        } else if (lexeme[j].equals("string")) {
+                            System.out.println("<" + lexeme[j].toString() + ">,<2>");
+                        } else if (lexeme[j].equals("if")) {
+                            System.out.println("<" + lexeme[j].toString() + ">,<3>");
+                        } else if (lexeme[j].equals("else")) {
+                            System.out.println("<" + lexeme[j].toString() + ">,<4>");
+                        } else if (lexeme[j].equals("do")) {
+                            System.out.println("<" + lexeme[j].toString() + ">,<5>");
+                        } else if (lexeme[j].equals("while")) {
+                            System.out.println("<" + lexeme[j].toString() + ">,<6>");
+                        } else if (lexeme[j].matches(
+                                "(a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|v|u|w|x|y|z).(a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|0|1|2|3|4|5|6|7|8|9)*.(a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|0|1|2|3|4|5|6|7|8|9)")) {
+                            System.out
+                                    .println("<" + lexeme[j].toString() + ">,<id>,<" + lexeme[j].toString() + ">");
+                        } else if (lexeme[j].matches(
+                                "\".*\"")) {
+                            System.out
+                                    .println("<" + lexeme[j].toString() + ">,<sl>,<" + lexeme[j].toString() + ">");
+                        } else if (lexeme[j].matches(
+                                "[0-9]+")) {
+                            System.out
+                                    .println("<" + lexeme[j].toString() + ">,<in>,<" + lexeme[j].toString() + ">");
+                        } else if (lexeme[j].equals("<")) {
+                            System.out
+                                    .println("<" + lexeme[j].toString() + ">,<ro>,<lt>");
+                        } else if (lexeme[j].equals("<=")) {
+                            System.out
+                                    .println("<" + lexeme[j].toString() + ">,<ro>,<le>");
+                        } else if (lexeme[j].equals("==")) {
+                            System.out
+                                    .println("<" + lexeme[j].toString() + ">,<ro>,<eq>");
+                        } else if (lexeme[j].equals("!=")) {
+                            System.out
+                                    .println("<" + lexeme[j].toString() + ">,<ro>,<ne>");
+                        } else if (lexeme[j].equals(">")) {
+                            System.out
+                                    .println("<" + lexeme[j].toString() + ">,<ro>,<gt>");
+                        } else if (lexeme[j].equals(">=")) {
+                            System.out
+                                    .println("<" + lexeme[j].toString() + ">,<ro>,<ge>");
+                        } else if (lexeme[j].equals("+")) {
+                            System.out
+                                    .println("<" + lexeme[j].toString() + ">,<ao>,<ad>");
+                        } else if (lexeme[j].equals("-")) {
+                            System.out
+                                    .println("<" + lexeme[j].toString() + ">,<ao>,<sb>");
+                        } else if (lexeme[j].equals("*")) {
+                            System.out
+                                    .println("<" + lexeme[j].toString() + ">,<ao>,<ml>");
+                        } else if (lexeme[j].equals("/")) {
+                            System.out
+                                    .println("<" + lexeme[j].toString() + ">,<ao>,<dv>");
+                        } else if (lexeme[j].equals("=")) {
+                            System.out
+                                    .println("<" + lexeme[j].toString() + ">,<oo>,<as>");
+                        } else if (lexeme[j].equals("(")) {
+                            System.out
+                                    .println("<" + lexeme[j].toString() + ">,<oo>,<op>");
+                        } else if (lexeme[j].equals(")")) {
+                            System.out
+                                    .println("<" + lexeme[j].toString() + ">,<oo>,<cp>");
+                        } else if (lexeme[j].equals("{")) {
+                            System.out
+                                    .println("<" + lexeme[j].toString() + ">,<oo>,<ob>");
+                        } else if (lexeme[j].equals("}")) {
+                            System.out
+                                    .println("<" + lexeme[j].toString() + ">,<oo>,<cb>");
+                        } else if (lexeme[j].equals(";")) {
+                            System.out
+                                    .println("<" + lexeme[j].toString() + ">,<oo>,<tr>");
+                        } else {
+                            System.out.println("not-recognized");
+                        }
+                    }
                 }
             }
             fileReader.close();
-        } catch (FileNotFoundException e) {
+        } catch (
+
+        FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
